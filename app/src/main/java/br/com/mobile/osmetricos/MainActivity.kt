d3c573c70621c4ea7bac4d65873512f4c4d66f33
Andredossantos96 <86696902+Andredossantos96@users.noreply.github.com>
2022-04-18 21:27:15 -0300

    App mobile

--- app/src/main/java/br/com/mobile/osmetricos/MainActivity.kt
+++ app/src/main/java/br/com/mobile/osmetricos/MainActivity.kt
@@ -1,11 +1,79 @@
 package br.com.mobile.osmetricos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
+import kotlinx.android.synthetic.main.login.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DebugActivity() {
+
+    private val context: Context get() = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        // encontra objeto pelo id
+        campo_imagem.setImageResource(R.drawable.mundo_login)
+
+        texto_login.text = getString(R.string.mensagem_login)
+
+
+
+        // evento no botao de login forma 1
+//        botao_login.setOnClickListener {
+//            val valorUsuario = campo_usuario.text.toString()
+//            val valorSenha = campo_senha.text.toString()
+//            Toast.makeText(this, "$valorUsuario : $valorSenha", Toast.LENGTH_LONG).show()
+//        }
+
+        // segunda forma: delegar para método
+        botao_login.setOnClickListener {onClickLogin() }
+
+
+    }
+
+    fun onClickLogin(){
+        val valorUsuario = campo_usuario.text.toString()
+        val valorSenha = campo_senha.text.toString()
+        Toast.makeText(context, "$valorUsuario ", Toast.LENGTH_LONG).show()
+
+
+        if  (valorUsuario != "aluno") {
+           // botao_login.setError("usuario invalido")
+            Toast.makeText(context, "usuario invalido", Toast.LENGTH_LONG).show()
+
+        }
+
+        if  (valorSenha != "impacta") {
+            Toast.makeText(context, "senha invalida", Toast.LENGTH_LONG).show()
+        }
+
+        // criar intent
+        val intent = Intent(context, TelaInicialActivity::class.java)
+        // colocar parâmetros (opcional)
+        val params = Bundle()
+        params.putString("usuario", valorUsuario)
+        intent.putExtras(params)
+
+        // enviar parâmetros simplificado
+        intent.putExtra("numero", 10)
+
+        // fazer a chamada
+        //startActivity(intent)
+
+        // fazer a chamada esperando resultado
+        startActivityForResult(intent, 1)
+
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            val result = data?.getStringExtra("result")
+            Toast.makeText(context, "$result", Toast.LENGTH_LONG).show()
+        }
     }
 }
